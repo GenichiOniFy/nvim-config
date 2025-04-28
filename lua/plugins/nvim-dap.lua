4783bc7cdeee85f41d72f22c9964fd000f9ab157
GenichiOniFy <artem.viktorovich.gurin@gmail.com>
2025-04-28 03:24:32 +0300

    Add python dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -9,12 +9,38 @@ return {
         local dapui = require("dapui")
 
         dapui.setup()
-
+        --DOTNET
         dap.adapters.coreclr = {
-            type = 'executable',
-            command = 'netcoredbg',
-            args = {'--interpreter=vscode'}
+            type = "executable",
+            command = "netcoredbg",
+            args = { "--interpreter=vscode" },
         }
+        --PYTHON
+        dap.adapters.python = function(cb, config)
+            if config.request == "attach" then
+                ---@diagnostic disable-next-line: undefined-field
+                local port = (config.connect or config).port
+                ---@diagnostic disable-next-line: undefined-field
+                local host = (config.connect or config).host or "127.0.0.1"
+                cb({
+                    type = "server",
+                    port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+                    host = host,
+                    options = {
+                        source_filetype = "python",
+                    },
+                })
+            else
+                cb({
+                    type = "executable",
+                    command = "python",
+                    args = { "-m", "debugpy.adapter" },
+                    options = {
+                        source_filetype = "python",
+                    },
+                })
+            end
+        end
 
         dap.configurations.cs = {
             {
@@ -22,37 +48,62 @@ return {
                 name = "launch - netcoredbg",
                 request = "launch",
                 program = function()
-                    return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+                    return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+                end,
+            },
+        }
+        dap.configurations.python = {
+            {
+                -- The first three options are required by nvim-dap
+                type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+                request = "launch",
+                name = "Launch file",
+
+                -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+                program = "${file}", -- This configuration will launch the current file if used.
+                pythonPath = function()
+                    -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+                    -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+                    -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+                    local cwd = vim.fn.getcwd()
+                    if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+                        return cwd .. "/venv/bin/python"
+                    elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+                        return cwd .. "/.venv/bin/python"
+                    else
+                        return "/usr/bin/python"
+                    end
                 end,
             },
         }
 
         dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
         local continue = function()
-			-- support for vscode launch.json is partial.
-			-- not all configuration options and features supported
-			if vim.fn.filereadable(".vscode/launch.json") then
-				require("dap.ext.vscode").load_launchjs()
-			end
-			dap.continue()
-		end
+            -- support for vscode launch.json is partial.
+            -- not all configuration options and features supported
+            if vim.fn.filereadable(".vscode/launch.json") then
+                require("dap.ext.vscode").load_launchjs()
+            end
+            dap.continue()
+        end
 
-        vim.keymap.set('n','<F2>', dap.toggle_breakpoint, { desc = "Toggle breakpoint"})
-        vim.keymap.set('n','<F9>', continue, { desc = "Continue"})
-        vim.keymap.set('n','<F10>', dap.step_over, { desc = "Step over"})
-        vim.keymap.set('n','<F11>', dap.step_into, { desc = "Step into"})
-        vim.keymap.set('n','<F12>', dap.step_out, { desc = "Step out"})
-    end
+        vim.keymap.set("n", "<F2>", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<F9>", continue, { desc = "Continue" })
+        vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Step over" })
+        vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Step into" })
+        vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Step out" })
+    end,
 }
